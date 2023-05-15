bc04dea1aa92342ad09f4cd18bd4929e3da0b278
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-05-16 02:18:49 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ryflex/map_page.dart';
+import 'package:ryflex/splash_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) =>
+    print("Firebase Initialization completed..."));
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
+
